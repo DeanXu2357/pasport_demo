6577d3685951b22e26b94c205aa5a4d9fdac36e4
py <jasugun0000@gmail.com>
2017-09-15 00:04:59 +0800

    暫時先這樣 明天再看看API為啥不行

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class ExampleTest extends TestCase
 {
@@ -17,5 +18,17 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function testDingo()
+    {
+        $actual = $this->get('/test');
+        $expect = 200;//'Dingo Ready';
+        $this->assertEquals($expect, $actual->status());
+        // $this->get('/test')->assertJson(
+        //     [
+        //         'res' => 'API work'
+        //     ]
+        // );
     }
 }
