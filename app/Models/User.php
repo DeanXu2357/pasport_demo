4a0c4f6080c6b5baa75216581235b4c898a6f35c
py <jasugun0000@gmail.com>
2017-09-24 17:49:31 +0800

    變更user model的位置

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
